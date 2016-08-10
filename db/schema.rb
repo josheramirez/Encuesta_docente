98b45f8af9b61d6d265e601d335d9b6123faff24
josheramirez <joseinformatico2015@gmail.com>
2016-08-10 12:17:14 -0400

    intento

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160807183107) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "profesors", force: :cascade do |t|
     t.string   "nombre"
     t.string   "email"
